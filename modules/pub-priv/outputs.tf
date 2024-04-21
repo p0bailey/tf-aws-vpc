a7ba318e8e0fcb867c9decc9c82361ac375b6107
p0bailey <phillip@bailey.st>
2024-04-21 14:46:17 +0100

    Commit on 2024-04-21 14:46

--- modules/pub-priv/outputs.tf
+++ modules/pub-priv/outputs.tf
@@ -7,6 +7,11 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
+output "public_subnet_ids" {
+  description = "The IDs of the private subnets created"
+  value       = aws_subnet.public.*.id
+}
+
 output "private_subnet_ids" {
   description = "The IDs of the private subnets created"
   value       = aws_subnet.private.*.id
